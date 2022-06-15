86ffab7fa4e37aa8ffa36c1daa7b546ca98adf63
Franco Pipieri <pipierif98@gmail.com>
2022-06-15 20:05:03 -0300

    fetch

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+cargarProductos();
+
+
+function cargarProductos(){
+  console.log(fetch("product.data.json")
+    .then(function (response) { console.log(response)})
+    .then(json => console.log(json)));
+}
+
+function mostrarProductos(datos){
+  const div = document.querySelector(".items");
+  div.innerText = "";
+
+  datos.forEach(productoBody =>{
+    const {img, title, body, precio} = productoBody;
+
+      const card = document.createElement("div");
+      card.setAttribute("class","card text-white item");
+      card.innerHTML = ` <img src="${img}" class="card-img-top item-image" alt="Uniposca">
+                        <div class="card-body">
+                          <h5 class="card-title p-1 item-title">${title}</h5>
+                          <p class="card-text">${body}</p>
+                          <div class="item-details">
+                            <h6 class="item-price valor">${precio}</h6>
+                            <button class="boton btn agregarACarrito">Agregar</button>
+                          </div>
+                        </div>`;
+  })
+}
+
 class Item{
   constructor(id,itemTitle,itemPrice,itemImage,itemQuantity,itemColor,itemSize){
     this.id=id;
